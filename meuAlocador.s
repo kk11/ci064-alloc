735d0c885bc85876b51f7cde46113b6391b17ba4
Kevin Katzer <kk11@inf.ufpr.br>
2014-12-01 14:07:46 -0200

    Início do Trabalho :0
    Signed-off-by: Kevin Katzer <kk11@inf.ufpr.br>

--- meuAlocador.s
+++ meuAlocador.s
@@ -1,16 +1,41 @@
 .section .data
+
+heap_start:
+        .long 0
+
+current_break:
+        .long 0
+
+.equ HEADER_SIZE, 8
+.equ HDR_AVAIL_OFFSET, 0
+.equ HDR_SIZE_OFSSET, 4
+
+.equ UNAVAILABLE, 0
+.equ AVAILABLE, 1
+.equ BREAK, 45
+.equ SYSCALL, 0x80
+
 .section .text
+.globl initAloc
 .globl meuAlocaMem
 .globl meuLiberaMem
 .globl imprMapa
+
+.type initAloc, @funtion
+initAloc:
+        pushl %ebp
+        movl %esp, %ebp
+
+        movl BREAK, %eax
 
 .type meuAlocaMem, @function
 meuAlocaMem:
 	pushl %ebp
 	movl %esp, %ebp
 	movl 8(%ebp), %eax
-	#######Algoritmo#######
-	#return eax = endereço#
+
+        
+
 	popl %ebp
 	ret
 
