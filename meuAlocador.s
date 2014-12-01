72fe79737e2a61a72fa6ed3b1b58ad776fc8fec3
kevin <kevin@simepar.br>
2014-12-01 15:49:45 -0200

    Vou usar apenas vim pra parar de dar push
    Signed-off-by: kevin <kevin@simepar.br>

--- meuAlocador.s
+++ meuAlocador.s
@@ -40,83 +40,81 @@ meuAlocaMem:
   pushl %ebp
   movl %esp, %ebp
 
-  movl 8(%ebp), %ebx #ecx agora tem o tamanho a ser alocado
-  movl $1, %eax
-  int $SYSCALL
+  movl 8(%ebp), %ecx #ecx agora tem o tamanho a ser alocado
 
-#   movl heap_start, %eax #eax agora tem a localização atual da pesquisa
-#   movl current_break, %ebx
+  movl heap_start, %eax #eax agora tem a localização atual da pesquisa
+  movl current_break, %ebx
 
-# alloc_begin:
-#   cmpl %eax, %ebx #Procura chegou ao fim
-#   je more_mem
+alloc_begin:
+  cmpl %eax, %ebx #Procura chegou ao fim
+  je more_mem
 
-#   movl 4(%eax), %edx #edx = tamanho da seção de memória
-#   cmpl $UNAVAILABLE, 0(%eax) #isAvailable?
-#   je next_mem
+  movl 4(%eax), %edx #edx = tamanho da seção de memória
+  cmpl $UNAVAILABLE, 0(%eax) #isAvailable?
+  je next_mem
 
-#   cmpl %edx, %ecx #if ecx <= edx, aloca aqui
-#   jle alloc
+  cmpl %edx, %ecx #if ecx <= edx, aloca aqui
+  jle alloc
 
-# next_mem:
-#   addl 8, %eax
-#   addl %edx, %eax #Próxima seção de memória
+next_mem:
+  addl 8, %eax
+  addl %edx, %eax #Próxima seção de memória
 
-#   jmp alloc_begin
+  jmp alloc_begin
 
-# alloc:
-#   movl $UNAVAILABLE, 0(%eax) #Seta essa seção como ocupada
-#   movl %ecx, 4(%eax) #Seta o tamanho da seção
-#   addl 8, %eax
+alloc:
+  movl $UNAVAILABLE, 0(%eax) #Seta essa seção como ocupada
+  movl %ecx, 4(%eax) #Seta o tamanho da seção
+  addl 8, %eax
 
-#   movl %ebx, current_break
+  movl %ebx, current_break
 
-#   movl %ebp, %esp
-#   popl %ebp
-#   ret
+  movl %ebp, %esp
+  popl %ebp
+  ret
 
-# more_mem:
-#   addl 8, %ebx
-#   addl %ecx, %ebx #Adiciona o tanto de memória a mais que se precisa no break
+more_mem:
+  addl 8, %ebx
+  addl %ecx, %ebx #Adiciona o tanto de memória a mais que se precisa no break
 
-#   pushl %eax
-#   pushl %ebx
-#   pushl %ecx #salva os registradores para a chamada de função
+  pushl %eax
+  pushl %ebx
+  pushl %ecx #salva os registradores para a chamada de função
 
-#   movl $BREAK, %eax
-#   int $SYSCALL #Reseta o break
+  movl $BREAK, %eax
+  int $SYSCALL #Reseta o break
 
-#   cmpl $0, %eax
-#   je error #erro
+  cmpl $0, %eax
+  je error #erro
 
-#   popl %ecx
-#   popl %ebx
-#   popl %eax
+  popl %ecx
+  popl %ebx
+  popl %eax
 
-#   jmp alloc
+  jmp alloc
 
-# error:
-#   movl $0, %eax #Em erro, retorna 0
-#   movl %ebp, %esp
-#   popl %ebp
-#   ret
+error:
+  movl $0, %eax #Em erro, retorna 0
+  movl %ebp, %esp
+  popl %ebp
+  ret
 
-# .type meuLiberaMem, @function
-# meuLiberaMem:
-#   pushl %ebp
-#   movl %esp, %ebp
-#   movl 8(%ebp), %eax
-#   movl (%eax), %ebx
-#   #######Algoritmo#######
-#   movl %ebp, %esp
-#   popl %ebp
-#   ret
+.type meuLiberaMem, @function
+meuLiberaMem:
+  pushl %ebp
+  movl %esp, %ebp
+  movl 8(%ebp), %eax
+  movl (%eax), %ebx
+  #######Algoritmo#######
+  movl %ebp, %esp
+  popl %ebp
+  ret
 
-# .type imprMapa, @function
-# imprMapa:
-#   pushl %ebp
-#   movl %esp, %ebp
-#   ###Algoritmo###
-#   movl %ebp, %esp
-#   popl %ebp
-#   ret
+.type imprMapa, @function
+imprMapa:
+  pushl %ebp
+  movl %esp, %ebp
+  ###Algoritmo###
+  movl %ebp, %esp
+  popl %ebp
+  ret
